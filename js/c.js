961ea0698641ca63883d723f95caa7ff9538655b
Dhruv verma <85825906+Dhruvermafz@users.noreply.github.com>
2022-09-18 12:12:06 +0530

    Update c.js

--- js/c.js
+++ js/c.js
@@ -1,7 +1,7 @@
 let questions = [
     {
     numb: 1,
-    question: "1. Which is correct with respect to the size of the data types in C?",
+    question: "Which is correct with respect to the size of the data types in C?",
     answer: "char < int < float",
     options: [
       "char > int > float",
@@ -12,7 +12,7 @@ let questions = [
   },
     {
     numb: 2,
-    question: "2.C Language was developed in which year?",
+    question: "C Language was developed in which year?",
     answer: "1970",
     options: [
       "1970",
@@ -23,7 +23,7 @@ let questions = [
   },
     {
     numb: 3,
-    question: " 3.Which one of the following is not a reserved keyword for C?",
+    question: "Which one of the following is not a reserved keyword for C?",
     answer: "main",
     options: [
       "auto",
@@ -34,7 +34,7 @@ let questions = [
   },
     {
       numb: 4,
-    question: "4. When we mention the prototype of a function?",
+    question: "When we mention the prototype of a function?",
     answer: "​​​Declaring ",
     options: [
       "Defining",
@@ -45,8 +45,8 @@ let questions = [
   },
     {
       numb: 5,
-      question: "5.Out of the following snippet, which one will generate random numbers effectively?",
-      answer: " rand();",
+      question: "Out of the following snippet, which one will generate random numbers effectively?",
+      answer: "rand();",
       options: [
         " rand(time(NULL));",
         " rand(10);",
@@ -56,7 +56,7 @@ let questions = [
   },
   {
     numb: 6,
-    question: "6.  The #include <stdio.h> is a?",
+    question: "The #include <stdio.h> is a?",
     answer: "Preprocessor directive",
     options: [
       "Inclusion directive",
@@ -67,7 +67,7 @@ let questions = [
 },
 {
   numb: 7,
-  question: "7. Which of the following is an exit controlled loop?",
+  question: "Which of the following is an exit controlled loop?",
   answer: "do-while loop.",
   options: [
     "While loop",
@@ -78,7 +78,7 @@ let questions = [
 },
 {
   numb: 8,
-  question: "8. Which of the following are not standard header files in C?",
+  question: "Which of the following are not standard header files in C?",
   answer: "None of the above",
   options: [
     "stdio.h",
@@ -89,7 +89,7 @@ let questions = [
 },
 {
   numb: 9,
-  question: "9. What is the return type of the fopen() function in C?",
+  question: "What is the return type of the fopen() function in C?",
   answer: "Pointer to a FILE object",
   options: [
     "Pointer to a FILE object",
@@ -100,7 +100,7 @@ let questions = [
 },
 {
   numb: 10,
-  question: "10. Which of the following is not a storage class specifier in C?",
+  question: "Which of the following is not a storage class specifier in C?",
   answer: "volatile",
   options: [ 
     "volatile",
