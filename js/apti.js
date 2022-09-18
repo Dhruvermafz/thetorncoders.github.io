9f8e3affad5f194dff78ebdb51e41e81bbc5fe3c
Dhruv verma <85825906+Dhruvermafz@users.noreply.github.com>
2022-09-18 12:10:41 +0530

    Update apti.js

--- js/apti.js
+++ js/apti.js
@@ -1,7 +1,7 @@
 let questions = [
     {
     numb: 1,
-    question: "1.At what rate percent per annum will a sum of money double in 8 years?",
+    question: "At what rate percent per annum will a sum of money double in 8 years?",
     answer: "12.5%",
     options: [
       "12.5%",
@@ -12,7 +12,7 @@ let questions = [
   },
     {
     numb: 2,
-    question: "2.The sum required to earn a monthly interest of Rs 400 at 10 % per annum at simple interest is?",
+    question: "The sum required to earn a monthly interest of Rs 400 at 10 % per annum at simple interest is?",
     answer: " Rs 2000",
     options: [
       " Rs 2000",
@@ -23,7 +23,7 @@ let questions = [
   },
     {
     numb: 3,
-    question: " 3. If A's height is 40% less than that of B, how much percent B's height is more than that of A?",
+    question: "If A's height is 40% less than that of B, how much percent B's height is more than that of A?",
     answer: " 66.66%",
     options: [
       " 66.66%",
@@ -34,7 +34,7 @@ let questions = [
   },
     {
       numb: 4,
-    question: "4. Which e-learning platform has been featured in Time Magazine's 2021 list of 100 most influential companies?",
+    question: "Which e-learning platform has been featured in Time Magazine's 2021 list of 100 most influential companies?",
     answer: "BYJU's",
     options: [
       "BYJU's",
@@ -45,7 +45,7 @@ let questions = [
   },
     {
       numb: 5,
-      question: "5. The cost price of 20 articles is the same as the selling price of x articles. If the profit is 25%, then the value of x is?",
+      question: "The cost price of 20 articles is the same as the selling price of x articles. If the profit is 25%, then the value of x is?",
       answer: "16",
       options: [
         "15",
@@ -56,7 +56,7 @@ let questions = [
   },
   {
     numb: 6,
-    question: "6.  A clock is started at noon. By 10 minutes past 5, the hour hand has turned through?",
+    question: "A clock is started at noon. By 10 minutes past 5, the hour hand has turned through?",
     answer: "155°",
     options: [
       "145°",
@@ -67,7 +67,7 @@ let questions = [
 },
 {
   numb: 7,
-  question: "7. What was the day of the week on 28th May, 2006?",
+  question: "What was the day of the week on 28th May, 2006?",
   answer: "Sunday",
   options: [
     "Thursday",
@@ -78,7 +78,7 @@ let questions = [
 },
 {
   numb: 8,
-  question: "8. The average of 20 numbers is zero. Of them, at the most, how many may be greater than zero?",
+  question: "The average of 20 numbers is zero. Of them, at the most, how many may be greater than zero?",
   answer: "19",
   options: [
     "0",
@@ -89,7 +89,7 @@ let questions = [
 },
 {
   numb: 9,
-  question: "9. The banker's gain of a certain sum due 2 years hence at 10% per annum is Rs. 24. The present worth is?",
+  question: "The banker's gain of a certain sum due 2 years hence at 10% per annum is Rs. 24. The present worth is?",
   answer: "Rs. 600",
   options: [
     "Rs. 480",
@@ -100,7 +100,7 @@ let questions = [
 },
 {
   numb: 10,
-  question: "10.What least number must be added to 1056, so that the sum is completely divisible by 23 ?",
+  question: "What least number must be added to 1056, so that the sum is completely divisible by 23 ?",
   answer: "2",
   options: [ 
     "2",
