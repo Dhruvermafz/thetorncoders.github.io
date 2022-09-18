d6241e0ba94a67d75709320d0416f40641122068
Dhruv verma <85825906+Dhruvermafz@users.noreply.github.com>
2022-09-18 15:17:22 +0530

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,43 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyANa2BuSy9Jg5jyRTNtgh-Ye3WXYGEAcr8",
+    authDomain: "thetorncoders.firebaseapp.com",
+    databaseURL: "https://thetorncoders-default-rtdb.firebaseio.com",
+    projectId: "thetorncoders",
+    storageBucket: "thetorncoders.appspot.com",
+    messagingSenderId: "139286346534",
+    appId: "1:139286346534:web:6393a88e75f0e40e2f9fe7"
+  };
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// ===== Firebase configuration (end) ==== //
+
+
+var loginFormDB = firebase.database().ref("loginForm");
+
+document.getElementById("loginForm").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+  var email = getElementVal("email");
+  var pass = getElementVal("pass");
+
+  saveMessages(email, pass);
+  //   reset the form
+  document.getElementById("loginForm").reset();
+}
+
+const saveMessages = (email, pass) => {
+  var newloginForm = loginFormDB.push();
+
+  newloginForm.set({
+    email: email,
+    pass: pass
+  });
+};
+
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+};
+
---
+++ js/register.js
@@ -0,0 +1,52 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyANa2BuSy9Jg5jyRTNtgh-Ye3WXYGEAcr8",
+    authDomain: "thetorncoders.firebaseapp.com",
+    databaseURL: "https://thetorncoders-default-rtdb.firebaseio.com",
+    projectId: "thetorncoders",
+    storageBucket: "thetorncoders.appspot.com",
+    messagingSenderId: "139286346534",
+    appId: "1:139286346534:web:6393a88e75f0e40e2f9fe7"
+  };
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// ===== Firebase configuration (end) ==== //
+
+var regFormDB = firebase.database().ref("regForm");
+
+document.getElementById("regForm").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  var name = getElementVal("name");
+  var email = getElementVal("email");
+  var address = getElementVal("address");
+  var age = getElementVal("age");
+  var pass = getElementVal("pass");
+
+  saveMessages(name, email, age, address, pass);
+
+ 
+
+  //   reset the form
+  document.getElementById("regForm").reset();
+}
+
+const saveMessages = (name, email, age, address, pass) => {
+  var newregForm = regFormDB.push();
+
+  newregForm.set({
+    name: name,
+    email: email,
+    age: age,
+    address: address,
+    pass: pass
+  });
+};
+
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+};
+
